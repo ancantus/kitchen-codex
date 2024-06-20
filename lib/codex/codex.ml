bbaa64fa45ecb3f03061deb5195dfec4972819a2
Ryan Estep <rmestep@bex.net>
2024-06-21 09:14:50 +1200

    Refactored unit tests to use caqti libs. App still to come.
    Notes on implementaton:
    - Absolute pain on OUnit2 and LWT runtime both launching threads? Would
      get deadlocks with multiple tests using LWT (and thought it was sqlite
      for a long time).
    - Caqti interface is awkard for multiple chained queries. Not sure if
      I'm using it right here.

--- lib/codex/codex.ml
+++ lib/codex/codex.ml
@@ -126,7 +126,7 @@ let find_or_create_meal db meal =
                 "SELECT id FROM meals WHERE name = ?;" in
         match meals with
                 | [] -> create_meal db meal
-                | meal_id :: _ -> meal_id
+                | meal_id :: _ -> meal_id 
 
 let write_meal_plan db (plan:Types.meal_plan) =
         let existing_plan = find_meal_plan_index db plan.date in 
@@ -163,14 +163,14 @@ let load_meal_plan date =
         let meal_plan = read_meal_plan db date in
         let _ = Sqlite3.db_close db in
         meal_plan
-
+(*
 let update_meal_plan plan =
         let db = open_db () in
         write_meal_plan db plan;
         let updated_plan = read_meal_plan db plan.date in
         let _ = Sqlite3.db_close db in
         updated_plan
-
+*)
 let search_meal search_str =
         let db = open_db () in
         let canidate_meals = fuzzy_match_meals db search_str in
@@ -185,3 +185,148 @@ let clear () =
         let _ = Sqlite3.db_close db in
         ()
 
+open Caqti_request.Infix
+module type DB = Caqti_lwt.CONNECTION
+module R = Caqti_request
+module T = Caqti_type
+module E = Caqti_error
+module Ccal = Caqti_type_calendar
+
+(* lwt runner junk: move to a utils package *)
+let run_caqti sql_uri callback =
+        let%lwt result = Caqti_lwt_unix.with_connection (Uri.of_string sql_uri) callback in
+        Caqti_lwt.or_fail result
+
+(* Database Migration code *)
+let to_caqti_query str = Caqti_query.of_string str |> Result.get_ok
+let migrations = List.map to_caqti_query [
+        "CREATE TABLE IF NOT EXISTS versions (
+                codex_version INTEGER NOT NULL, 
+                last_modified TEXT NOT NULL
+        );";
+        "CREATE TABLE IF NOT EXISTS meals (
+                id INTEGER PRIMARY KEY ASC, 
+                name TEXT UNIQUE,
+                last_modified TEXT NOT NULL
+        );";
+        "CREATE TABLE IF NOT EXISTS plans (
+                id INTEGER PRIMARY KEY ASC, 
+                date TEXT UNIQUE NOT NULL,
+                breakfast INTEGER references meals(id),
+                lunch INTEGER references meals(id),
+                dinner INTEGER references meals(id)
+        );";
+]
+
+(* This was the only way I could figure out to capture this specific error enum *)
+type no_version_err = [
+        | `Request_failed of E.query_error
+]
+let migrate_target () =
+        let max_db_version = List.length migrations - 1 in
+        let table_exists_query =
+                let open R.Infix in (T.string ->? T.string)
+                "SELECT name from sqlite_schema WHERE type='table' and name=$1;" in
+        let get_version_query =
+                let open R.Infix in (T.unit ->! T.int)
+                "SELECT codex_version FROM versions;" in
+        fun (module Db: DB) ->
+                let%lwt version_table_exists = Db.find_opt table_exists_query "versions" in
+                let%lwt current_version = Db.find get_version_query () in
+                let target = match Result.get_ok version_table_exists with None -> Some 0 (* special case for first migration step that creates this version table *)
+                        | _ -> let version = Result.get_ok current_version in if version < max_db_version then (Some (version + 1)) else None 
+                in
+                Lwt.return target
+
+(* Helper function to unwrap the result type that's the usual return of Caqti db functions and throw / terminate on any errors *)
+let caqti_ok r =
+        match%lwt r with 
+                | Ok r_val -> r_val 
+                | Error e -> E.show e |> failwith 
+let caqti_ok_unit r = 
+        match%lwt r with
+                | Ok _ -> Lwt.return_unit
+                | Error e -> E.show e |> failwith
+       
+let migrate_db version =
+        let update_version_query =
+                let open R.Infix in (T.(t2 int Ccal.ctime) -->. T.unit)
+                (* This needs to be specalized for version 0 to insert into the table instead of updating *)
+                (fun _ -> (match version with 
+                                | 0 -> "INSERT INTO versions (codex_version, last_modified) VALUES ($1, $2);" 
+                                | _ -> "UPDATE versions SET codex_version = $1, last_modified = $2;")
+                           |> to_caqti_query)
+        in
+        let migrate_query =
+                let open R.Infix in (T.unit -->. T.unit)
+                (fun _ -> List.nth migrations version) in
+        fun (module Db: DB) ->
+                let%lwt _ = Db.exec migrate_query () |> caqti_ok_unit in
+                let%lwt _ = Db.exec update_version_query (version, CalendarLib.Calendar.now ()) |> caqti_ok_unit in
+                Lwt.return version
+
+let migrate_to_latest () =
+        let rec inner (module Db: DB) = 
+                let%lwt target = (migrate_target ()) (module Db) in
+                print_endline ("Upgrading to target: " ^ (match target with Some i -> Int.to_string i | None -> "Done!!"));
+                match target with 
+                        | None -> Lwt.return_unit 
+                        | Some version -> let%lwt _ = (migrate_db version) (module Db) in inner (module Db);
+        in
+        inner
+
+(* caqti meal index processing *)
+let cmeal_index = T.(option string)
+let cmeal_plan = 
+        let encode (plan: Types.meal_plan) =
+                (plan.date, plan.breakfast, plan.lunch, plan.dinner) in 
+        let decode plan_tuple : Types.meal_plan = match plan_tuple with (date, breakfast, lunch, dinner) -> {date; breakfast; lunch; dinner}
+        in
+        T.(product decode @@ proj (t4 Ccal.cdate cmeal_index cmeal_index cmeal_index) encode @@ proj_end)
+
+let get_meal_plan date = 
+        let query = 
+        let open R.Infix in (Ccal.cdate ->! cmeal_plan)
+        "SELECT p.date, b.name, l.name, d.name 
+         FROM plans as p 
+         LEFT JOIN meals b ON b.id = p.breakfast 
+         LEFT JOIN meals l ON l.id = p.lunch
+         LEFT JOIN meals d ON d.id = p.dinner
+         WHERE date = $1 LIMIT 1" in
+        fun (module Db: DB) -> 
+                let result = Db.find_opt query date in
+                Lwt.bind result (fun u -> match u with
+                                | Ok opt_u -> Lwt.return(Ok (Option.value opt_u ~default:{date=date; breakfast=None; lunch=None; dinner=None}))
+                                | Error e -> Lwt.return(Error e))
+let get_meal_id name = 
+        let query =
+        let open R.Infix in (T.(option string) ->? T.int)
+        "SELECT id FROM meals WHERE name = $1;" in
+        fun (module Db: DB) -> Db.find_opt query name 
+
+let find_or_create_meal name =
+        let query = 
+        let open R.Infix in T.(t2 T.string Ccal.ctime ->. T.unit)
+        "INSERT INTO meals (name, last_modified) VALUES ($1, $2);" in
+        fun (module Db: DB) ->
+                let%lwt meal_id = match%lwt (get_meal_id name) (module Db) with Ok id -> Lwt.return id | Error e -> failwith (E.show e) in
+                match meal_id with
+                        | Some id -> Lwt.return id (* early return of existing meal *)
+                        | None -> (
+                                        (* otherwise we're creating a new meal! *)
+                                        let%lwt _ = Db.exec query (Option.get name, CalendarLib.Calendar.now ()) in 
+                                        match%lwt (get_meal_id name) (module Db) with
+                                                | Ok id -> Option.get id |> Lwt.return
+                                                | Error e -> failwith (E.show e)
+        )
+
+let update_meal_plan (plan: Types.meal_plan) = 
+        let query =
+        let open R.Infix in (T.(t4 Ccal.cdate (option int) (option int) (option int)) ->. T.unit)
+                "INSERT OR REPLACE INTO plans (date, breakfast, lunch, dinner) VALUES ($1, $2, $3, $4);" in
+        fun (module Db: DB) ->
+                let%lwt breakfast_id = (find_or_create_meal plan.breakfast) (module Db) in
+                let%lwt lunch_id = (find_or_create_meal plan.lunch) (module Db) in
+                let%lwt dinner_id = (find_or_create_meal plan.dinner) (module Db) in
+                Db.exec query (plan.date, Some breakfast_id, Some lunch_id, Some dinner_id)
+ 
--- test/test_codex.ml
+++ test/test_codex.ml
@@ -1,4 +1,5 @@
 open OUnit2
+open OUnitLwt
 
 (* OUnit2 equality assert for the custom meal_plan type *) 
 let assert_meal_plan_equal = assert_equal ~cmp:Types.meal_plan_eq ~printer:Types.meal_plan_to_str
@@ -8,24 +9,37 @@ let empty_meal_plan date = {Types.date=date; Types.breakfast=None; Types.lunch=N
 let assert_meal_plan_empty date = assert_meal_plan_equal (empty_meal_plan date)
 
 
-let tests_load_meal_plan = "meal plan test suite" >::: [
-        "load empty plan" >:: (fun _ -> 
-                Codex.clear (); 
-                let date = CalendarLib.Date.make 2024 1 1 in 
-                assert_meal_plan_empty date (Codex.load_meal_plan date));
-        "add meal plan" >:: (fun _ -> 
-                Codex.clear ();
-                let date = CalendarLib.Date.make 2024 1 1 in
-                let expected_meal_plan = {Types.date=date; Types.breakfast=Some "Groats"; Types.lunch=Some "Ham Sandwich"; Types.dinner=Some "Tacos"} in
-                assert_meal_plan_equal expected_meal_plan (Codex.update_meal_plan expected_meal_plan));
-        "load existing meal plan" >:: (fun _ ->
-                Codex.clear();
-                let date = CalendarLib.Date.make 2024 1 1 in
-                let expected_meal_plan = {Types.date=date; Types.breakfast=Some "Groats"; Types.lunch=Some "Ham Sandwich"; Types.dinner=Some "Tacos"} in
-                let _ = Codex.update_meal_plan expected_meal_plan in
-                assert_meal_plan_equal expected_meal_plan (Codex.load_meal_plan date));
+module type DB = Caqti_lwt.CONNECTION
+let run_caqti_test callback =
+        let uuid = (Uuidm.v4_gen (Random.State.make_self_init ())) () in
+        let uri = Printf.sprintf "sqlite3:%s?mode=memory" (Uuidm.to_string uuid) in
+        Codex.run_caqti uri (fun (module Db: DB) -> 
+                let%lwt _ = (Codex.migrate_to_latest ()) (module Db) in
+                callback (module Db : DB)
+        ) 
+
+let tests_caqti_codex = "Caqti codex refactor" >::: [
+        "load empty meal plan" >:: (lwt_wrapper
+                (fun _ -> 
+                        let date = CalendarLib.Date.make 2024 1 1 in
+                        let%lwt meal_plan = run_caqti_test (Codex.get_meal_plan date) in
+                        Lwt.return (assert_meal_plan_empty date meal_plan)
+                )
+        );
+        "add meal plan" >:: (lwt_wrapper
+                (fun _ -> 
+                        let date = CalendarLib.Date.make 2024 1 1 in
+                        let expected_meal_plan = {Types.date=date; Types.breakfast=Some "Groats"; Types.lunch=Some "Ham Sandwich"; Types.dinner=Some "Tacos"} in
+                        let%lwt added_meal_plan = run_caqti_test (fun (module Db: DB) -> 
+                                let%lwt _ = (Codex.update_meal_plan expected_meal_plan) (module Db) in
+                                (Codex.get_meal_plan expected_meal_plan.date) (module Db))
+                        in
+                        Lwt.return (assert_meal_plan_equal expected_meal_plan added_meal_plan)
+                )
+        );
 ]
 
+
 let codex_tests = "codex test suite" >::: [
-        tests_load_meal_plan
+        tests_caqti_codex;
 ]
